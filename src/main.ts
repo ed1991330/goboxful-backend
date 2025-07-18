31ef5da64b15817a11bdbe9f140e6a6a37ea8c06
Eduardo Molina <eduardo.molina@pgs360.com>
2025-07-18 10:53:23 -0700

    backend se habilitan los CORNS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // ✅ permite solo tu frontend
+    credentials: true, // si usarás cookies/sesión
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
