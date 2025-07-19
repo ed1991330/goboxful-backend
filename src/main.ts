8d57d01fd5d2444053a2024678482cea84f17ee9
Eduardo Molina <eduardo.molina@pgs360.com>
2025-07-19 08:28:24 -0700

    finalizacion de poryecto de backedn

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000', // ✅ permite solo tu frontend
+    origin: 'http://localhost:3000', //  permite solo tu frontend
     credentials: true, // si usarás cookies/sesión
   });
   await app.listen(process.env.PORT ?? 3000);
